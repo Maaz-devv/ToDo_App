c805d55563c98f30b8d88d8647a1f66661e5cdfa
Maaz-devv <maazzahid011@gmail.com>
2025-06-02 13:10:13 +0500

    first commit

---
+++ TodoApp/TodoApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TodoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.taskGrid = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtTaskDescription = new System.Windows.Forms.TextBox();
+            this.btnComplete = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUnmark = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // taskGrid
+            // 
+            this.taskGrid.AllowUserToAddRows = false;
+            this.taskGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.taskGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.taskGrid.Location = new System.Drawing.Point(12, 58);
+            this.taskGrid.Name = "taskGrid";
+            this.taskGrid.ReadOnly = true;
+            this.taskGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.taskGrid.Size = new System.Drawing.Size(776, 300);
+            this.taskGrid.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(345, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtTaskDescription
+            // 
+            this.txtTaskDescription.Location = new System.Drawing.Point(12, 12);
+            this.txtTaskDescription.Name = "txtTaskDescription";
+            this.txtTaskDescription.Size = new System.Drawing.Size(300, 20);
+            this.txtTaskDescription.TabIndex = 2;
+            this.txtTaskDescription.TextChanged += new System.EventHandler(this.txtDescription_TextChanged);
+            // 
+            // btnComplete
+            // 
+            this.btnComplete.Location = new System.Drawing.Point(12, 390);
+            this.btnComplete.Name = "btnComplete";
+            this.btnComplete.Size = new System.Drawing.Size(75, 23);
+            this.btnComplete.TabIndex = 3;
+            this.btnComplete.Text = "Mark Done";
+            this.btnComplete.UseVisualStyleBackColor = true;
+            this.btnComplete.Click += new System.EventHandler(this.btnComplete_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(237, 390);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete Task";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUnmark
+            // 
+            this.btnUnmark.Location = new System.Drawing.Point(121, 390);
+            this.btnUnmark.Name = "btnUnmark";
+            this.btnUnmark.Size = new System.Drawing.Size(75, 23);
+            this.btnUnmark.TabIndex = 5;
+            this.btnUnmark.Text = "Unmark Done";
+            this.btnUnmark.UseVisualStyleBackColor = true;
+            this.btnUnmark.Click += new System.EventHandler(this.btnUnmark_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUnmark);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnComplete);
+            this.Controls.Add(this.txtTaskDescription);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.taskGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.taskGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView taskGrid;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtTaskDescription;
+        private System.Windows.Forms.Button btnComplete;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUnmark;
+    }
+}
+
---
+++ TodoApp/TodoApp/Form1.cs
@@ -0,0 +1,187 @@
+﻿
+using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Drawing;
+
+
+namespace TodoApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTasks(); // Load tasks when form opens
+            taskGrid.CellEndEdit += TaskGrid_CellEndEdit;
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["TodoDb"].ConnectionString;
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "INSERT INTO tasks (description, is_done) VALUES (@description, 0)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@description", txtTaskDescription.Text);
+                cmd.ExecuteNonQuery();
+            }
+
+            // Clear the textbox
+            txtTaskDescription.Text = "";
+
+            // Refresh the task list
+            LoadTasks();
+        }
+        private void TaskGrid_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            int id = Convert.ToInt32(taskGrid.Rows[e.RowIndex].Cells["id"].Value);
+            string newDescription = taskGrid.Rows[e.RowIndex].Cells["description"].Value.ToString();
+
+            string connStr = ConfigurationManager.ConnectionStrings["TodoDb"].ConnectionString;
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "UPDATE tasks SET description = @desc WHERE id = @id";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@desc", newDescription);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Task updated successfully.");
+        }
+
+
+        private void LoadTasks()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["TodoDb"].ConnectionString;
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "SELECT id, description, is_done, created_at FROM tasks";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                taskGrid.DataSource = table;
+
+                taskGrid.Columns["id"].HeaderText = "ID";
+                taskGrid.Columns["description"].HeaderText = "Task";
+                taskGrid.Columns["is_done"].HeaderText = "Done?";
+                taskGrid.Columns["created_at"].HeaderText = "Created";
+
+                // Make editable
+                taskGrid.ReadOnly = false;
+                taskGrid.Columns["id"].ReadOnly = true;
+                taskGrid.Columns["created_at"].ReadOnly = true;
+
+                // Auto-size columns
+                taskGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+                // Color completed tasks
+                foreach (DataGridViewRow row in taskGrid.Rows)
+                {
+                    bool isDone = Convert.ToBoolean(row.Cells["is_done"].Value);
+                    if (isDone)
+                    {
+                        row.DefaultCellStyle.BackColor = Color.LightGreen;
+                        row.DefaultCellStyle.Font = new Font(taskGrid.Font, FontStyle.Strikeout);
+                    }
+                }
+            }
+        }
+
+        private void txtDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnComplete_Click(object sender, EventArgs e)
+        {
+            if (taskGrid.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(taskGrid.SelectedRows[0].Cells["id"].Value);
+                string connStr = ConfigurationManager.ConnectionStrings["TodoDb"].ConnectionString;
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    string query = "UPDATE tasks SET is_done = 1 WHERE id = @id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+
+                LoadTasks();
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to mark as complete.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (taskGrid.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(taskGrid.SelectedRows[0].Cells["id"].Value);
+                string connStr = ConfigurationManager.ConnectionStrings["TodoDb"].ConnectionString;
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    string query = "DELETE FROM tasks WHERE id = @id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+
+                LoadTasks();
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.");
+            }
+        }
+
+        private void btnUnmark_Click(object sender, EventArgs e)
+        {
+            if (taskGrid.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a task to unmark.", "Info");
+                return;
+            }
+
+            // get the ID of the selected task
+            int id = Convert.ToInt32(taskGrid.SelectedRows[0].Cells["id"].Value);
+
+            string connStr = ConfigurationManager
+                .ConnectionStrings["TodoDb"]
+                .ConnectionString;
+
+            using (var conn = new MySql.Data.MySqlClient.MySqlConnection(connStr))
+            {
+                conn.Open();
+                string q = "UPDATE tasks SET is_done = 0 WHERE id = @id";
+                var cmd = new MySql.Data.MySqlClient.MySqlCommand(q, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+
+            // reload so the green/strikeout style is removed
+            LoadTasks();
+        }
+
+    }
+
+
+}
